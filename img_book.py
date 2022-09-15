c99d0ff4ba63628e2d3e127eb576aa72be6ff03e
BOGPC228 <77198939+BOGPC228@users.noreply.github.com>
2022-09-15 12:14:29 +0300

    download books with titles

--- img_book.py
+++ img_book.py
@@ -1,19 +1,56 @@
 import requests
 import os
 from pathlib import Path
+from bs4 import BeautifulSoup
+from pathvalidate import sanitize_filename
 
-file_path = "books"
-directory = os.path.dirname(file_path)
 
-try:
-    os.stat(file_path)
-except:
-    os.mkdir(file_path)
+def check_for_redirect(response):  
+    if response.history:
+        raise requests.HTTPError
 
-for i in range(1,11):
-    url = "https://tululu.org/txt.php?id=" + str(i)
+
+def parse_book_page(response):
+    soup = BeautifulSoup(response.text, 'lxml')
+    title_tag = soup.find(id='content').find('h1')
+    title_text = title_tag.text
+    name, author = title_text.split(" :: ")
+    name_book = name.strip()
+    author_book = author.strip()
+    filename = f"{name_book}.txt"
+    return filename
+
+
+def download_txt(url, filename, folder='books/'):
+    os.makedirs(folder, exist_ok=True)
     response = requests.get(url)
-    response.raise_for_status() 
-    filename = "books/id{}.txt".format(i)
-    with open(filename, 'wb') as file:
+    response.raise_for_status()
+    path = os.path.join(folder, sanitize_filename(filename))
+    with open(path, 'wb') as file:
         file.write(response.content)
+
+
+def main():
+    for book_number in range(1,11):
+        params = {"id": book_number}
+        loading_book_url = "https://tululu.org/txt.php"
+        book_response = requests.get(loading_book_url, params)
+        page_book = f'https://tululu.org/b{book_number}/'
+        try:
+            book_response.raise_for_status()
+            check_for_redirect(book_response)
+
+            page_response = requests.get(page_book)
+            page_response.raise_for_status()
+            check_for_redirect(page_response)
+            filename = parse_book_page(page_response)
+            print(filename)
+            download_txt(loading_book_url, filename)
+        except requests.HTTPError:
+            print("Такой книги нет")
+
+    
+
+
+if __name__ == "__main__":
+    main()
