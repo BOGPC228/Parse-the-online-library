cd191dd5149250ec6d4ec599b7dbe4426d0e8216
BOGPC228 <77198939+BOGPC228@users.noreply.github.com>
2023-05-18 11:31:56 +0300

    Исправил названия переменных

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -6,7 +6,7 @@ from check_for_redirect import check_for_redirect
 
 
 def get_urls_books(start_page, end_page):
-    books_url = []
+    book_urls = []
     for page in range(start_page, end_page):
         try:
             url = "https://tululu.org/l55/{}".format(page)
@@ -14,13 +14,13 @@ def get_urls_books(start_page, end_page):
             check_for_redirect(response)
             response.raise_for_status()
             soup = BeautifulSoup(response.text, 'lxml')
-            books_id = soup.find_all('table', class_="d_book")
-            for book_id in books_id:
+            book_ids = soup.find_all('table', class_="d_book")
+            for book_id in book_ids:
                 book_id = book_id.find('a')['href']
                 book_url = urljoin(url, book_id)
-                books_url.append(book_url)
+                book_urls.append(book_url)
     
-            return books_url
+            return book_urls
         except requests.HTTPError:
             print("Такой страницы нету")
         except requests.ConnectionError:
