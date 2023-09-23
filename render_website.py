0c560bdf11577544f8f9657eed905f7cf2f67beb
BOGPC228 <77198939+BOGPC228@users.noreply.github.com>
2023-09-23 13:38:56 +0300

    Split books list by pages

--- render_website.py
+++ render_website.py
@@ -1,28 +1,41 @@
-from jinja2 import Template, Environment, FileSystemLoader
+import os
 import json
+import math
 
 from livereload import Server
-
+from jinja2 import Template, Environment, FileSystemLoader
+from more_itertools import chunked
 
 def rebuild():
     with open("media/book_parse.json", 'r', encoding="utf-8") as json_file:
         books = json.load(json_file)
 
+    books_count = len(books)
+    book_pages = list(chunked(books, 20))
+    count_pages = math.ceil(books_count/20)
 
-    env = Environment(loader=FileSystemLoader("."))
-    template = env.get_template("template.html")
-    render_html = template.render(books=books)
+    for page_number, page_books in enumerate(book_pages, start=1):
+        env = Environment(loader=FileSystemLoader("."))
+        template = env.get_template("template.html")
+        render_html = template.render(books=page_books, 
+                                      current_page=page_number,
+                                      total_pages=count_pages)
 
-
-    with open("index.html", 'w', encoding="utf-8") as html_file:
-        html_file.write(render_html)
+        with open(F"pages/index{page_number}.html", 'w', encoding="utf-8") as html_file:
+            html_file.write(render_html)
     
     print("Site rebuilt")
 
-rebuild()
 
-server = Server()
+def main():
+    os.makedirs("pages", exist_ok=True)
+    
+    rebuild()
 
-server.watch('template.html', rebuild)
+    server = Server()
+    server.watch('template.html', rebuild)
+    server.serve(root='.')
 
-server.serve(root='.')
+
+if __name__ == "__main__":
+    main()
